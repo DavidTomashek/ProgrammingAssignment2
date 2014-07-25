cae4a741291d337bcc2361b8e497e8fc38352327
davetoe <davetoe@comcast.net>
2014-07-24 17:21:10 -0700

    First Draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
+## 
 
-## Write a short comment describing this function
+## mt
 
-makeCacheMatrix <- function(x = matrix()) {
+## The makeVector function from the example, as written,
+## can actually work to cache any function result, not just mean.
+## I pasted it here and changed the variable names to reflect
+## the more generalized purpose of the function.
+## The assignment wants us to name it makeCacheMatrix, but
+## this could as easily be named makeCache and be used for caching
+## any function results that work on a numeric vector
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setfctn <- function(fctn) m <<- fctn
+  getfctn <- function() m
+  list(set = set, get = get,
+       setfctn = setfctn,
+       getfctn = getfctn)
+}
 
+## cacheSolve interacts with makeCacheMatrix and makes use of
+## the functions in its list.
+## cacheSolve uses getfctn() to test if the cache variable has a result stored 
+## if so, it returns the cached value
+## otherwise it calculates the inverse matrix then
+## loads the results in the cache variable and returns the variable value
+cacheSolve <- function(x = matrix()) {
+      m <- x$getfctn()
+      if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+      }
+      data <- x$get()
+      m <- solve(data)
+      x$setfctn(m)
+      m
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
